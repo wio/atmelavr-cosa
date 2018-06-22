2d2fef2d69b9e3eb38ffa7044a314563b42daaa6
Deep Dhillon <deep@deepdhillon.ca>
2018-06-22 14:33:03 -0400

    files added for the framework

---
+++ README.md
@@ -0,0 +1,20 @@
+# AtmelAVR Cosa Framework
+
+Atmel AVR 8- and 32-bit MCUs deliver a unique combination of performance, power efficiency and design flexibility. Optimized to speed time to market-and easily adapt to new ones-they are based on the industrys most code-efficient architecture for C and assembly programming.
+
+[More Information about Atmel AVR](http://www.microchip.com/design-centers/8-bit/avr-mcus) 
+
+Cosa is an object-oriented platform for Arduino. It replaces the Arduino and Wiring library with a large set of integrated classes that support the full range of AVR/ATmega/ATtiny internal hardware modules; all pin modes, Digital, and Analog Pins, External and Pin Change Interrupts, Analog Comparator, PWM, Watchdog, Timer0/Timer2 (RTT), Timer1 (Servo/Tone/VWI), Input Capture, UART, USI, SPI, TWI and EEPROM. Cosa supports several programming paradigms including Multi-Tasking, Event Driven Programming and UML Capsules/Actors.
+
+[More Information about Cosa](https://github.com/mikaelpatel/cosa)
+
+## Wio Framework 
+This is a framework supported by Wio and is compatible with `atmelavr` platform. To use this framework:
+* [Install wio](https://github.com/wio/wio#install)
+* Create an AtmelAVR package or AtmelAVR app
+```bash
+wio create pkg --platform atmelavr:cosa
+```
+```bash
+wio create app atmelavr cosa
+```
---
+++ cmake/CosaToolchain.cmake
@@ -0,0 +1,47 @@
+if (COSA_IS_TOOLCHAIN_PROCESSED)
+    return()
+endif ()
+set(COSA_IS_TOOLCHAIN_PROCESSED True)
+set(CMAKE_SYSTEM_NAME Cosa)
+
+# Cosa requires C++11 standard
+set(CMAKE_CXX_STANDARD 11)
+
+# Set compilers
+set(CMAKE_C_COMPILER avr-gcc)
+set(CMAKE_ASM_COMPILER avr-gcc)
+set(CMAKE_CXX_COMPILER avr-g++)
+
+# Include Cosa.cmake in module path
+if (EXISTS ${CMAKE_CURRENT_LIST_DIR}/Platform/Cosa.cmake)
+    message(STATUS "Setting module path with Cosa.cmake")
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
+endif ()
+
+# Include system paths
+if (UNIX)
+    message(STATUS "Including Unix paths")
+    include(Platform/UnixPaths)
+    if (APPLE)
+        message(STATUS "Including Darwin paths")
+        list(APPEND CMAKE_SYSTEM_PREFIX_PATH ~/Applications
+                /Applications
+                /Developer/Applications
+                /sw         # Fink
+                /opt/local) # MacPorts
+    endif ()
+elseif (WIN32)
+    message(STATUS "Including Windows paths")
+    include(Platform/WindowsPaths)
+endif ()
+
+# Set platform path to Cosa and arduino-cmake
+set()
+
+if (NOT ${ARDUINO_CMAKE_PATH})
+    message(FATAL_ERROR "ARDUINO_CMAKE_PATH is not defined")
+endif()
+
+set(COSA_SDK_PATH "${CMAKE_CURRENT_LIST_DIR}/../core")
+message(STATUS "`cosa`          location: ${COSA_SDK_PATH}")
+message(STATUS "`arduino-cmake` location: ${ARDUINO_CMAKE_PATH}")
